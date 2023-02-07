6e7d7ce7d78e21082d02a77049a46391e900a42b
ivanmunoz99 <ivandariomunos99@gmail.com>
2023-02-07 16:31:04 -0500

    button display correction

--- src/components/layouts/header/Header.jsx
+++ src/components/layouts/header/Header.jsx
@@ -12,15 +12,15 @@ function Header() {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse " id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <ul className="navbar-nav ms-auto mb-lg-0 p-3">
                         <form className="d-flex ms-auto" role="search">
                             <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" />
                                 <button className="btn btn-outline-success" type="submit">Buscar</button>
                         </form>
-                            <li className="nav-item">
+                            <li className="nav-item ms-auto">
                                 <a className="nav-link active" aria-current="page" href="/">Iniciar sesión</a>
                             </li>
-                            <li className="nav-item">
+                            <li className="nav-item ms-auto">
                                 <a className="nav-link" href="/">Registrarse</a>
                             </li>
                         </ul>
