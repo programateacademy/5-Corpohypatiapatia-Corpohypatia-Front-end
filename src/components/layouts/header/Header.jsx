963fe38ac1e130339905cf4cc1b39b220ef78742
ivanmunoz99 <ivandariomunos99@gmail.com>
2023-02-08 18:26:12 -0500

    logo

--- src/components/layouts/header/Header.jsx
+++ src/components/layouts/header/Header.jsx
@@ -1,12 +1,12 @@
-
+import logo from '../../../assets/img/logo.jpg'
 
 function Header() {
     return (
         <>
             <nav className="navbar navbar-expand-md bg-body-tertiary bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">
-                        <img src="" alt="corpohypatia" />
+                    <a className="navbar-brand p-0" href="/">
+                        <img src={logo} width="70" alt="corpohypatia" />
                     </a>
                     <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
