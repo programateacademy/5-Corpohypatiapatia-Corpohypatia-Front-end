c8675fae692eb0cdb25e1abf39435850475b4a72
ivanmunoz99 <ivandariomunos99@gmail.com>
2023-02-09 22:34:36 -0500

    searcher

--- src/components/layouts/header/Header.jsx
+++ src/components/layouts/header/Header.jsx
@@ -1,6 +1,42 @@
 import logo from '../../../assets/img/Logo.jpg'
+import React, {useState, useEffect} from 'react';
 
 function Header() {
+    // hooks
+    const [info, setInfo] = useState([])
+    const [search, setSearch] = useState("")
+
+    //api or db
+    const URL = "https://jsonplaceholder.typicode.com/users"
+
+    //datos a traer
+    const dataSheet = async () => {
+        const res = await fetch(URL)
+        const data = await res.json()
+        setInfo(data);
+        console.log(data);
+    }
+    
+    //function search
+    const searcher = (e) =>{
+        setSearch(e.target.value)
+        console.log(e.target.value)
+    }
+
+    //filter
+    let result = []
+    if(!search){
+        result = info
+    }else{
+        result = info.filter((dato) => dato.name.toLowerCase().includes(search.toLocaleLowerCase()))
+    }
+
+    console.log(result)
+
+    useEffect(() =>{
+        dataSheet();
+    },[]);
+
     return (
         <>
             <nav className="navbar navbar-expand-md bg-body-tertiary bg-light">
@@ -14,7 +50,7 @@ function Header() {
                     <div className="collapse navbar-collapse " id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-lg-0 p-3">
                         <form className="d-flex ms-auto" role="search">
-                            <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" />
+                            <input value={search} onChange={searcher} className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" />
                                 <button className="btn btn-outline-success" type="submit">Buscar</button>
                         </form>
                             <li className="nav-item ms-auto">
