af1689150fbb5fd4a51c465a9c502c5b0407298c
Bryan0175 <bryanskate0175@hotmail.com>
2023-02-13 11:07:00 -0500

    sidebar in desktop without responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Login from "./pages/login/Login";
 import Register from "./pages/userRegister/UserRegister";
+import Side from "./components/layouts/sidebar/Sidebar";
 
 function App() {
   return (
     <div>
-
+      <Side/>
     </div>
   );
 }
--- src/components/layouts/sidebar/Sidebar.jsx
+++ src/components/layouts/sidebar/Sidebar.jsx
@@ -1,18 +1,91 @@
-function Sidebar() {
-    const handleLogout = () => {
-        localStorage.removeItem("token");
-        window.location.reload();
-    };
+import React, { useState } from "react";
+import "./sidebar.css";
+import logo from "../../../assets/img/CorpoHypatia.png";
+import logo2 from "../../../assets/img/brandReduce-CorpoHypatia.png";
+
+import {
+    FaBars,
+    FaThLarge,
+} from "react-icons/fa";
+import {
+    FiLogOut
+} from "react-icons/fi";
+import {
+    IoIosArrowForward,
+    IoIosArrowDown
+} from "react-icons/io";
+import {
+    AiFillHome
+} from "react-icons/ai";
+import {
+    ImFolderOpen,
+    ImFolderPlus,
+    ImUserPlus
+} from "react-icons/im";
+import {
+    HiUsers,
+    HiUserGroup,
+} from "react-icons/hi";
+
+const Sidebar = () => {
+    const [openMenu, setOpenMenu] = useState(true);
+    const [openDP, setOpenDP] = useState(false);
 
     return (
-        <div>
-            <nav>
-                <button onClick={handleLogout}>
-                    Logout
-                </button>
-            </nav>
-        </div>
+        <nav className="navbar">
+            <div className="logo">
+                <div className="toggleMenu">
+                    <label>Administrador</label>
+                    <FaBars onClick={() => setOpenMenu(!openMenu)} className="menuBars" size={25}/>
+                </div>
+                <img src={logo} width={150}/>
+                <div className="info">
+                    <h3>Usuario</h3>
+                    <label>Activo</label>
+                </div>
+            </div>
+            <ul className="nav-links">
+                <div className="menu">
+                    <li className="home"><a href="/">
+                        <AiFillHome className="iconBar"/>
+                        Home
+                    </a></li>
+                    <li><a href="/">
+                        <FaThLarge className="iconBar"/>
+                        Proyectos
+                    </a>{openDP ? <IoIosArrowForward/> : <IoIosArrowDown/>}</li>
+                    <ul className="subMenu">
+                        <li><a>
+                            <ImFolderOpen className="subIconos"/>
+                            Lista de proyectos
+                        </a></li>
+                        <li><a>
+                            <ImFolderPlus className="subIconos"/>
+                            Crear Proyectos
+                        </a></li>
+                    </ul>
+                    <li><a href="/">
+                        <HiUserGroup className="iconBar"/>
+                        Usuarios
+                    </a>{openDP ? <IoIosArrowForward/> : <IoIosArrowDown/>}
+                    </li>
+                    <ul className="subMenu">
+                        <li><a>
+                            <HiUsers className="subIconos"/>
+                            Lista de usuarios
+                        </a></li>
+                        <li><a>
+                            <ImUserPlus className="subIconos"/>
+                            Crear Usuarios
+                        </a></li>
+                    </ul>
+                </div>
+            </ul>
+            <div className="logout">
+                <button>Cerrar sesion <FiLogOut/></button>
+            </div>
+        </nav>
     );
-}
+};
 
 export default Sidebar;
