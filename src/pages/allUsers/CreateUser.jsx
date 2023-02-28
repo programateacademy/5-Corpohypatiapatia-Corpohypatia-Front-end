5dbee8add847eaa9d4151b1f84e7c19b1c66029d
Jonathan Sánchez <jondsancheza@gmail.com>
2023-02-28 08:55:54 -0500

    Cambios listar usuarios

--- src/pages/allUsers/CreateUser.jsx
+++ src/pages/allUsers/CreateUser.jsx
@@ -7,17 +7,28 @@ const URI = 'http://localhost:8000/users/';
 const CreateUser = () => {
     const [name,setName] = useState('');
     const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [phone,setPhone] = useState('');
+    const [position,setPosition] = useState('');
+    const [profile,setProfile] = useState('');
 
     const navigate = useNavigate();
 
     const store = async (e) => {
         e.preventDefault();
-        await axios.post(URI, {name:name, email:email});
+        await axios.post(URI, {
+            name:name, 
+            email:email,
+            password: password,
+            phone: phone,
+            position: position,
+            profile: profile
+        });
         navigate('/users');        
     }
     
     return (
-        <div>
+        <div className="container mt-5">
             <h3>Crear Usuario</h3>
             <form onSubmit={store}>
                 <div className="mb-3">
@@ -34,6 +45,42 @@ const CreateUser = () => {
                     <input 
                         value={email}
                         onChange ={ (e) => setEmail(e.target.value)}
+                        type="email"
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Contraseña</label>
+                    <input 
+                        value={password}
+                        onChange ={ (e) => setPassword(e.target.value)}
+                        type="password"
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Teléfono</label>
+                    <input 
+                        value={phone}
+                        onChange ={ (e) => setPhone(e.target.value)}
+                        type="text"
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Posición</label>
+                    <input 
+                        value={position}
+                        onChange ={ (e) => setPosition(e.target.value)}
+                        type="text"
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Perfil</label>
+                    <input 
+                        value={profile}
+                        onChange ={ (e) => setProfile(e.target.value)}
                         type="text"
                         className="form-control"
                     />
--- src/pages/allUsers/EditUser.jsx
+++ src/pages/allUsers/EditUser.jsx
@@ -7,6 +7,10 @@ const URI = 'http://localhost:8000/users/';
 const EditUser = () => {
     const [name,setName] = useState('');
     const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+    const [phone,setPhone] = useState('');
+    const [position,setPosition] = useState('');
+    const [profile,setProfile] = useState('');
     const navigate = useNavigate();
     const {id} = useParams()
 
@@ -14,7 +18,11 @@ const EditUser = () => {
         e.preventDefault();
         await axios.put(`${URI}${id}`, {
             name: name,
-            email: email
+            email: email,
+            password: password,
+            phone: phone,
+            position: position,
+            profile: profile
         });
         navigate('/users');
     }
@@ -27,10 +35,14 @@ const EditUser = () => {
         const res = await axios.get(`${URI}${id}`);
         setName(res.data.name);
         setEmail(res.data.email);
+        setPassword(res.data.password);
+        setPhone(res.data.phone);
+        setPosition(res.data.position);
+        setProfile(res.data.profile);
     }
 
     return(
-        <div>
+        <div className='container mt-5'>
             <h3>Editar Usuario</h3>
             <form onSubmit={update}>
                 <div className="mb-3">
@@ -47,6 +59,42 @@ const EditUser = () => {
                     <input 
                         value={email}
                         onChange ={ (e) => setEmail(e.target.value)}
+                        type="text"
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Contraseña</label>
+                    <input 
+                        value={password}
+                        onChange ={ (e) => setPassword(e.target.value)}
+                        type="password"
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Teléfono</label>
+                    <input 
+                        value={phone}
+                        onChange ={ (e) => setPhone(e.target.value)}
+                        type="text"
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Posición</label>
+                    <input 
+                        value={position}
+                        onChange ={ (e) => setPosition(e.target.value)}
+                        type="text"
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Perfil</label>
+                    <input 
+                        value={profile}
+                        onChange ={ (e) => setProfile(e.target.value)}
                         type="text"
                         className="form-control"
                     />
