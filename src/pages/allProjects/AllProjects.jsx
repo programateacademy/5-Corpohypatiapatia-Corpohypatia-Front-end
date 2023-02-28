be927dbd61cfbe00e2b11e606d45add762b8a868
Angela Diaz <angeldi846@gmail.com>
2023-02-28 02:20:13 -0500

    update

--- src/pages/allProjects/AllProjects.jsx
+++ src/pages/allProjects/AllProjects.jsx
@@ -1,16 +1,12 @@
 import { useState, useEffect } from "react";
 import ReactPaginate from "react-paginate";
-import CardProject from "../../components/common/cardProject/CardProject";
+import CardProjects from "../../components/common/cardProject/CardProjects";
 import { Link } from "react-router-dom";
 
 import './allProjects.css'
-// import Projects from "../../components/common/cardProject/projects"
 import { FaSearch } from 'react-icons/fa'
-// import { RxDoubleArrowRight, RxDoubleArrowLeft } from 'react-icons/rx'
 
 import { getProjects } from "../../service/api";
-import Sidebar from "../../components/layouts/Sidebar/Sidebar";
-import { SLayout, SMain } from "../../components/layouts/Layout/styles";
 
 function AllProjects() {
 
@@ -28,8 +24,6 @@ function AllProjects() {
         setProjects(response.data);
     };
 
-    console.log(projects)
-
     const searchedProject = projects.filter((item) => {
         if (search.value === "") return item;
 
@@ -41,7 +35,7 @@ function AllProjects() {
     // Pagination
     const [pageNumber, setPageNumber] = useState(0);
 
-    const projectPerPage = 8;
+    const projectPerPage = 6;
 
     const visitedPage = pageNumber * projectPerPage;
 
@@ -56,54 +50,46 @@ function AllProjects() {
         setPageNumber(selected);
     };
 
-
     return (
-        <SLayout>
-            <Sidebar />
-            <SMain>
-                <section className="all-project">
+        <section className="all-project">
 
-                    <h1>Proyectos</h1>
+            <section className="filters">
+                <div className='search'>
+                    <input
+                        placeholder='Busca por el nombre'
+                        type="text"
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                    <FaSearch className='search_icon' />
+                </div>
+            </section>
 
-                    <section className="filters">
-                        <div className='search'>
-                            <input
-                                placeholder='Busca por el nombre'
-                                type="text"
-                                value={search}
-                                onChange={(e) => setSearch(e.target.value)}
-                            />
-                            <FaSearch className='search_icon' />
-                        </div>
-                    </section>
+            <section>
+                <div className='projects-cards'>
+                    {displayPage.map((item) => (
+                        <Link to={`/projects/${item._id}`}>
+                        <CardProjects
+                            key={item.id}
+                            item={item}
+                        /></Link>
+                    ))}
+                </div>
 
-                    <section>
-                        <div className='projects-cards'>
-                            {displayPage.map((item) => (
-                                <Link to={`/admin-projects/${item._id}`}>
-                                    <CardProject
-                                        key={item.id}
-                                        item={item}
-                                    /></Link>
-                            ))}
-                        </div>
-
-                        <div className="container_paginate">
-                            <ReactPaginate
-                                pageCount={pageCount}
-                                onPageChange={changePage}
-                                previousLabel="Anterior"
-                                previousClassName="previousBttns"
-                                nextLabel="Siguiente"
-                                nextClassName="previousBttns"
-                                containerClassName="paginationBttns"
-                                activeClassName={"active_pagination"}
-                            />
-                        </div>
-                    </section>
-                </section>
-            </SMain>
-        </SLayout>
+                <div className="container_paginate">
+                    <ReactPaginate
+                        pageCount={pageCount}
+                        onPageChange={changePage}
+                        previousLabel="Anterior"
+                        previousClassName="previousBttns"
+                        nextLabel="Siguiente"
+                        nextClassName="previousBttns"
+                        containerClassName="paginationBttns"
+                        activeClassName={"active_pagination"}
+                    />
+                </div>
+            </section>
+        </section>
     );
 }
 
