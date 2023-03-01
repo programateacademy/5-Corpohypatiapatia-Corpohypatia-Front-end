7a81395eff7f5266aa7db52ad5a6a7c9d3d4da4e
ivanmunoz99 <ivandariomunos99@gmail.com>
2023-03-01 13:16:22 -0500

    map data

--- src/pages/form/Data.jsx
+++ src/pages/form/Data.jsx
@@ -1,81 +1,88 @@
 import React from 'react'
 
-function Data() {
+function Data({datos}) {
     return (
         <div className='container my-3'>
             <table className='table table-striped align-middle' >
-                <thead >
-                    <tr className='table-success'>
-                        <th className='text-center'>Datos de la Entidad SOLICITANTE</th>
-                        <th className='text-center'>info</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th>Nombre entidad</th>
-                        <th></th>
-                    </tr>
-                    <tr>
-                        <th>Dirección</th>
-                        <th></th>
-                    </tr>
-                    <tr>
-                        <th>Página web</th>
-                        <th></th>
-                    </tr>
-                    <tr>
-                        <th>Teléfono</th>
-                        <th></th>
-                    </tr>
-                </tbody>
-                <thead>
-                    <tr className='table-success'>
-                        <th className='text-center'>Contacto de la Persona</th>
-                        <th className='text-center'>info</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th >Nombre</th>
-                        <th></th>
-                    </tr>
-                    <tr>
-                        <th>Teléfono</th>
-                        <th></th>
-                    </tr>
-                    <tr>
-                        <th>Correo electronico</th>
-                        <th></th>
-                    </tr>
-                </tbody>
-                <thead>
-                    <tr className='table-success'>
-                        <th className='text-center'>Datos de la Entidad</th>
-                        <th className='text-center'>info</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th >Estatus legal</th>
-                        <th></th>
-                    </tr>
-                    <tr>
-                        <th>País</th>
-                        <th></th>
-                    </tr>
-                    <tr>
-                        <th>Año de fundación</th>
-                        <th></th>
-                    </tr>
-                    <tr>
-                        <th>NIT</th>
-                        <th></th>
-                    </tr>
-                </tbody>
-                <div className='d-grid gap-2 d-md-block'>
-                    <button type="submit" className="btn btn-outline-warning m-3" onClick={() => }>Editar</button>
-                    <button type="submit" className="btn btn-outline-danger m-3" onClick={() => }>Eliminar</button>
-                </div>
+                {
+                    datos.map((item) => (
+                        <>
+
+                            <thead key={item}>
+                                <tr className='table-success'>
+                                    <th className='text-center'>Datos de la Entidad SOLICITANTE</th>
+                                    <th className='text-center'>info</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>Nombre entidad</th>
+                                    <th>{item.entity_name}</th>
+                                </tr>
+                                <tr>
+                                    <th>Dirección</th>
+                                    <th>{item.entity_adress}</th>
+                                </tr>
+                                <tr>
+                                    <th>Página web</th>
+                                    <th>{item.entity_webpage}</th>
+                                </tr>
+                                <tr>
+                                    <th>Teléfono</th>
+                                    <th>{item.entity_phone}</th>
+                                </tr>
+                            </tbody>
+                            <thead>
+                                <tr className='table-success'>
+                                    <th className='text-center'>Contacto de la Persona</th>
+                                    <th className='text-center'>info</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th >Nombre</th>
+                                    <th>{item.contact_name}</th>
+                                </tr>
+                                <tr>
+                                    <th>Teléfono</th>
+                                    <th>{item.contact_phone}</th>
+                                </tr>
+                                <tr>
+                                    <th>Correo electronico</th>
+                                    <th>{item.contact_email}</th>
+                                </tr>
+                            </tbody>
+                            <thead>
+                                <tr className='table-success'>
+                                    <th className='text-center'>Datos de la Entidad</th>
+                                    <th className='text-center'>info</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th >Estatus legal</th>
+                                    <th>{item.legal_status}</th>
+                                </tr>
+                                <tr>
+                                    <th>País</th>
+                                    <th>{item.country}</th>
+                                </tr>
+                                <tr>
+                                    <th>Año de fundación</th>
+                                    <th>{item.foundaion_year}</th>
+                                </tr>
+                                <tr>
+                                    <th>NIT</th>
+                                    <th>{item.registry_nit}</th>
+                                </tr>
+                            </tbody>
+                            <div className='d-grid gap-2 d-md-block'>
+                                <button type="submit" className="btn btn-outline-warning m-3" onClick={() => console.log()}>Editar</button>
+                                <button type="submit" className="btn btn-outline-danger m-3" onClick={() => console.log()}>Eliminar</button>
+                            </div>
+                        </>
+                    ))
+                }
             </table>
         </div>
     )
--- src/pages/form/EntityForm.jsx
+++ src/pages/form/EntityForm.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import Data from './Data'
 
 function EntityForm() {
     //data db
@@ -48,7 +49,7 @@ function EntityForm() {
     }
 
     //update data
-    const handleUpdate = async (e) =>{
+    const handleUpdate = async (e) => {
         e.preventDefault();
         await axios.put(``, {
             entity_name,
@@ -67,48 +68,51 @@ function EntityForm() {
     }
 
     //remove data
-    const handleRemove = async (_id) =>{
-        await axios.delete (``)
+    const handleRemove = async (_id) => {
+        await axios.delete(``)
         datosBack()
     }
 
     return (
-        <div className='container'>
-            <form>
-                <div className="m-3 px-5 py-3 bg-body-secondary">
-                    <div className='fs-3 mb-2 text-center'>Datos de la Entidad Solicitante</div>
-                    <label for="nombreEntidad" className="form-label ">Nombre de la entidad</label>
-                    <input type="text" id="nombreEntidad" className="form-control mb-4" placeholder="nombre entidad" onChange={(e) => setEntity_name(e.target.value)} value={entity_name} required />
-                    <label for="direccion" className="form-label">Dirección</label>
-                    <input type="text" id="direccion" className="form-control mb-4" placeholder="cll ejemplo # " onChange={(e) => setEntity_adress(e.target.value)} value={entity_adress} required />
-                    <label for="web" className="form-label">Página web</label>
-                    <input type="text" id="web" className="form-control mb-4" placeholder="www.ejemplo.com" onChange={(e) => setEntity_webpage(e.target.value)} value={entity_webpage} required />
-                    <label for="telefono" className="form-label">Teléfono</label>
-                    <input type="text" id="telefono" className="form-control mb-4" placeholder="numero" onChange={(e) => setEntity_phone(e.target.value)} value={entity_phone} required />
-                </div>
-                <div className="m-3 px-5 py-3 bg-body-secondary">
-                    <div className='fs-3 mb-2 text-center'>Contacto</div>
-                    <label for="nombre" className="form-label ">Nombre</label>
-                    <input type="text" id="nombre" className="form-control mb-4" placeholder="nombre" onChange={(e) => setContact_name(e.target.value)} value={contact_name} required />
-                    <label for="tel" className="form-label">Teléfono</label>
-                    <input type="text" id="tel" className="form-control mb-4" placeholder="numero" onChange={(e) => setContact_phone(e.target.value)} value={contact_phone} required />
-                    <label for="email" className="form-label">correo electronico</label>
-                    <input type="email" id="email" className="form-control mb-4" placeholder="@ejemplo.com" onChange={(e) => setContact_email(e.target.value)} value={contact_email} required />
-                </div>
-                <div className="m-3 px-5 py-3 bg-body-secondary">
-                    <div className='fs-3 mb-2 text-center'>Datos de la Entidad</div>
-                    <label for="legal" className="form-label ">Estatus legal</label>
-                    <input type="text" id="legal" className="form-control mb-4" placeholder="" onChange={(e) => setLegal_status(e.target.value)} value={legal_status} required />
-                    <label for="pais" className="form-label">País</label>
-                    <input type="text" id="pais" className="form-control mb-4" placeholder="" onChange={(e) => setCountry(e.target.value)} value={country} required />
-                    <label for="ano" className="form-label">Año de fundación</label>
-                    <input type="text" id="ano" className="form-control mb-4" placeholder="yy/mm/dd" onChange={(e) => setFoundaion_year(e.target.value)} value={foundaion_year} required />
-                    <label for="nit" className="form-label">NIT</label>
-                    <input type="text" id="nit" className="form-control mb-4" placeholder="" onChange={(e) => setRegistry_nit(e.target.value)} value={registry_nit} required />
+        <>
+            <div className='container'>
+                <form>
+                    <div className="m-3 px-5 py-3 bg-body-secondary">
+                        <div className='fs-3 mb-2 text-center'>Datos de la Entidad Solicitante</div>
+                        <label for="nombreEntidad" className="form-label ">Nombre de la entidad</label>
+                        <input type="text" id="nombreEntidad" className="form-control mb-4" placeholder="nombre entidad" onChange={(e) => setEntity_name(e.target.value)} value={entity_name} required />
+                        <label for="direccion" className="form-label">Dirección</label>
+                        <input type="text" id="direccion" className="form-control mb-4" placeholder="cll ejemplo # " onChange={(e) => setEntity_adress(e.target.value)} value={entity_adress} required />
+                        <label for="web" className="form-label">Página web</label>
+                        <input type="text" id="web" className="form-control mb-4" placeholder="www.ejemplo.com" onChange={(e) => setEntity_webpage(e.target.value)} value={entity_webpage} required />
+                        <label for="telefono" className="form-label">Teléfono</label>
+                        <input type="text" id="telefono" className="form-control mb-4" placeholder="numero" onChange={(e) => setEntity_phone(e.target.value)} value={entity_phone} required />
+                    </div>
+                    <div className="m-3 px-5 py-3 bg-body-secondary">
+                        <div className='fs-3 mb-2 text-center'>Contacto</div>
+                        <label for="nombre" className="form-label ">Nombre</label>
+                        <input type="text" id="nombre" className="form-control mb-4" placeholder="nombre" onChange={(e) => setContact_name(e.target.value)} value={contact_name} required />
+                        <label for="tel" className="form-label">Teléfono</label>
+                        <input type="text" id="tel" className="form-control mb-4" placeholder="numero" onChange={(e) => setContact_phone(e.target.value)} value={contact_phone} required />
+                        <label for="email" className="form-label">correo electronico</label>
+                        <input type="email" id="email" className="form-control mb-4" placeholder="@ejemplo.com" onChange={(e) => setContact_email(e.target.value)} value={contact_email} required />
+                    </div>
+                    <div className="m-3 px-5 py-3 bg-body-secondary">
+                        <div className='fs-3 mb-2 text-center'>Datos de la Entidad</div>
+                        <label for="legal" className="form-label ">Estatus legal</label>
+                        <input type="text" id="legal" className="form-control mb-4" placeholder="" onChange={(e) => setLegal_status(e.target.value)} value={legal_status} required />
+                        <label for="pais" className="form-label">País</label>
+                        <input type="text" id="pais" className="form-control mb-4" placeholder="" onChange={(e) => setCountry(e.target.value)} value={country} required />
+                        <label for="ano" className="form-label">Año de fundación</label>
+                        <input type="text" id="ano" className="form-control mb-4" placeholder="yy/mm/dd" onChange={(e) => setFoundaion_year(e.target.value)} value={foundaion_year} required />
+                        <label for="nit" className="form-label">NIT</label>
+                        <input type="text" id="nit" className="form-control mb-4" placeholder="" onChange={(e) => setRegistry_nit(e.target.value)} value={registry_nit} required />
 
-                </div>
-            </form>
-        </div>
+                    </div>
+                </form>
+            </div>
+            <Data datos={datos} />
+        </>
     )
 }
 
