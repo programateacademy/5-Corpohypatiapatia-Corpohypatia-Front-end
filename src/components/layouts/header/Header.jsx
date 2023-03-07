530069d7af61fe1f9d3e8604fa1c70c9fd40e4aa
Andreinavlqz <andreinafloresvlqz54@gmail.com>
2023-03-07 15:37:59 -0500

    arrangement of navbar, buttons and title

--- src/components/layouts/header/Header.jsx
+++ src/components/layouts/header/Header.jsx
@@ -1,65 +1,91 @@
-import logo from "../../../assets/img/logo.jpg"
-import React, {useState, useEffect} from 'react';
+import logo from "../../../assets/img/logo.jpg";
+import React, { useState, useEffect } from "react";
+import "./header.css";
 
 function Header() {
-    // hooks
-    const [info, setInfo] = useState([])
-    const [search, setSearch] = useState("")
+  // hooks
+  const [info, setInfo] = useState([]);
 
-    //api or db
-    const URL = "https://jsonplaceholder.typicode.com/users"
+  //api or db
+  const URL = "https://jsonplaceholder.typicode.com/users";
 
-    //datos a traer
-    const dataSheet = async () => {
-        const res = await fetch(URL)
-        const data = await res.json()
-        setInfo(data);
-        console.log(data);
-    }
-    
-    //function search
-    const searcher = (e) =>{
-        setSearch(e.target.value)
-        console.log(e.target.value)
-    }
+  //datos a traer
+  const dataSheet = async () => {
+    const res = await fetch(URL);
+    const data = await res.json();
+    setInfo(data);
+    console.log(data);
+  };
 
-    //filter
-    let result = []
-    if(!search){
-        result = info
-    }else{
-        result = info.filter((dato) => dato.name.toLowerCase().includes(search.toLocaleLowerCase()))
-    }
+  useEffect(() => {
+    dataSheet();
+  }, []);
 
-    useEffect(() =>{
-        dataSheet();
-    },[]);
-
-    return (
-        <>
-            <nav className="navbar navbar-expand-md bg-body-tertiary bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand p-0" href="/">
-                        <img src={logo} width="200" alt="corpohypatia" />
-                    </a>
-                    <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse " id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auto mb-lg-0 p-3">
-                        <form className="d-flex ms-auto" role="search">
-                            <input value={search} onChange={searcher} className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" />
-                                <button className="btn btn-outline-success" type="submit">Buscar</button>
-                        </form>
-                            <li className="nav-item ms-auto">
-                                <a className="nav-link active" aria-current="page" href="/login">Iniciar sesión</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </>
-    );
+  return (
+    <>
+      <nav className="navbar navbar-expand-md bg-body-tertiary bg-light justify-content-center text-center">
+        <div className="container-fluid">
+          <a className="navbar-brand p-0" href="/">
+            <img src={logo} width="200" alt="corpohypatia" />
+          </a>
+          <button
+            className="navbar-toggler "
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav mb-lg-0 p-3 flex-column flex-sm-row justify-content-center">
+              <li className="nav-item">
+                <a
+                  className="nav-link active fw-bold"
+                  aria-current="page"
+                  href="/nosotros-proyectos-colaboradores"
+                >
+                  <b>Nosotros</b>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link active fw-bold"
+                  aria-current="page"
+                  href="/proyectos-colaboradores"
+                >
+                  <b>Proyectos</b>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a
+                  className="nav-link active fw-bold"
+                  aria-current="page"
+                  href="/colaboradores"
+                >
+                  <b>Colaboradores</b>
+                </a>
+              </li>
+              <li className="nav-item ms-sm-3">
+                <a
+                  className="nav-link active btn-purple"
+                  aria-current="page"
+                  href="/login"
+                >
+                  Iniciar sesión
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Header;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,73 +1,79 @@
 // import image for carousel
-import Graphic from '../../assets/img/grafico.png'
-import logo from '../../assets/img/logo.jpg'
-import organi from '../../assets/img/organigrama.png'
-import onu from '../../assets/img/min-onu.png'
-import './home.css'
-import Header from '../../components/layouts/header/Header'
-import Footer from '../../components/layouts/footer/Footer'
+import Graphic from "../../assets/img/grafico.png";
+import logo from "../../assets/img/logo.jpg";
+import "./home.css";
+import Header from "../../components/layouts/header/Header";
+import Footer from "../../components/layouts/footer/Footer";
+import Card from "react-bootstrap/Card";
 
 function Home() {
-    return (
-        <>
-            <Header />
-            <div className="container-fluid">
-                <div className="row p-5">
-                    <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 px-2">
-                        <p className="fs-1 mt-5">BIENVENID@S</p>
-                        <p className=' mt-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque earum repudiandae velit ipsa molestiae perferendis quae voluptate, adipisci et porro qui unde ad? Asperiores, accusamus tenetur obcaecati eos velit delectus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus maxime totam a? Laborum quasi nulla corporis alias ducimus. Ad obcaecati cupiditate ab enim molestiae unde quibusdam eligendi dignissimos tenetur. Incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta vel aut consequatur beatae officia vitae veniam, eos recusandae sequi autem quibusdam dolore asperiores? Sequi, aspernatur odit? Dolorem laboriosam cupiditate ad?</p>
-                        {/* buttons */}
-                        <div className="mt-5">
-                            <button type="button" class="btn btn-outline-secondary btn-lg">Continuar</button>
-                        </div>
-                    </div>
-                    <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 ">
-                        <div className="graphic-image">
-                            <img src={Graphic} alt="graphic piece" width="700" className="rounded mx-auto d-block" />
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <Header />
+      <div className="container-fluid">
+        <div className="row p-5">
+          <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 px-2">
+            <p className="fs-1 mt-5">BIENVENID@S</p>
+            <p className=" mt-5">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Doloremque earum repudiandae velit ipsa molestiae perferendis quae
+              voluptate, adipisci et porro qui unde ad? Asperiores, accusamus
+              tenetur obcaecati eos velit delectus? Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Possimus maxime totam a? Laborum
+              quasi nulla corporis alias ducimus. Ad obcaecati cupiditate ab
+              enim molestiae unde quibusdam eligendi dignissimos tenetur.
+              Incidunt. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Soluta vel aut consequatur beatae officia vitae veniam, eos
+              recusandae sequi autem quibusdam dolore asperiores? Sequi,
+              aspernatur odit? Dolorem laboriosam cupiditate ad?
+            </p>
+            {/* buttons */}
+            <div className="mt-5">
+              <button type="button" class="btn btn-lg btn-continuar">
+                Continuar
+              </button>
             </div>
-            {/* about company */}
-            <div className="container-fluid">
-                {/* row 2 */}
-                <div className="row px-5">
-                    <div className="col-sm-12 col-md-3 col-lg-3 col-xl-3">
-                        <p className="fs-3 text-center">Misión</p>
-                        <p className=''>
-                            Somos una organización que promueve y defiende los derechos humanos con enfoques diferenciales, en la búsqueda de la igualdad social y de género, la construcción de una sociedad democrática, pacífica y respetuosa de las diferencias, que garantice el buen vivir de hombres y mujeres en su
-                            diversidad.
-                        </p>
-                        <div className="graphicLogo">
-                            <img src={logo} alt="graphic piece" width={250} className="rounded mx-auto d-block my-5" />
-                        </div>
-                    </div>
-                    <div className="col-sm-12 col-md-5 col-lg-5 col-xl-5">
-                        <div className="ratio ratio-16x9 mb-5">
-                            <iframe src="" frameborder="0"></iframe>
-                        </div>
-                        <p className="fs-3 text-center">¿Quiénes Somos?</p>
-                        <p className="mb-5">La Corporación para la Equidad, la Democracia y el Buen Vivir - CORPORACIÓN HYPATIA, es una organización no gubernamental, sin ánimo de lucro, con sede en Bucaramanga (Santander) - Colombia.
-                            Su objeto social es la promoción y defensa integral de los derechos humanos, especialmente los derechos de las mujeres, la búsqueda de la equidad social y de género, el fortalecimiento de la gobernabilidad y de una sociedad democrática, contribuyendo a la construcción de una Cultura de paz y a la prevención y erradicación de toda forma de discriminación y vulneración de derechos por razones de sexo, etnia, orientación sexual, edad y condición socioeconómica.
-                        </p>
-                    </div>
-                    <div className="col-sm-12 col-md-4 col-lg-4 col-xl-4">
-                        <p className="fs-3 text-center">Visión</p>
-                        <p>
-                            La Corporación Hypatia es reconocida por su capacidad de influir en los derechos humanos, la democracia, la equidad y el buen vivir de hombres y mujeres en su diversidad.
-                        </p>
-                        <div className="organi my-5">
-                            <img src={organi} alt="organigrama" width={250} className="rounded mx-auto d-block" />
-                        </div>
-                    </div>
-                </div>
-                <div className="onu">
-                    <img src={onu} alt="asociaciones" className='img-fluid' />
-                </div>
+          </div>
+          <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 ">
+            <div className="graphic-image">
+              <img
+                src={Graphic}
+                alt="graphic piece"
+                width="700"
+                className="rounded mx-auto d-block"
+              />
             </div>
-            <Footer />
-        </>
-    );
+          </div>
+        </div>
+      </div>
+
+      {/* Title and search */}
+      <div className="row px-5 py-3 bg-purple text-purple align-items-center my-4">
+        <div className="col-12 text-center">
+          <h1 className="text-center text-purple display-4 mb-4">Proyectos</h1>
+        </div>
+      </div>
+
+      <div className="container-fluid">
+        <div className="row justify-content-start mb-3">
+          <div className="col-md-2 col-sm-2">
+            <div className="input-group input-group-sm">
+              <input
+                type="text"
+                className="form-control border-purple"
+                placeholder="Buscar proyectos"
+              />
+              <span className="input-group-text bg-purple border-purple">
+                <i className="bi bi-search text-white"></i>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
 }
 
 export default Home;
