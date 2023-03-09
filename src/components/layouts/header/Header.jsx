45b1dd9cd1e9af3acfd0bb4bfb3bd187faa1c960
felipe veloza <ciberronaldo@hotmail.com>
2023-03-09 10:31:15 -0500

    proyect cards and header fix

--- src/components/layouts/header/Header.jsx
+++ src/components/layouts/header/Header.jsx
@@ -1,10 +1,12 @@
 import logo from "../../../assets/img/logo.jpg";
 import React, { useState, useEffect } from "react";
 import "./header.css";
+import { Link } from "react-router-dom";
 
 function Header() {
   // hooks
   const [info, setInfo] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
 
   //api or db
   const URL = "https://jsonplaceholder.typicode.com/users";
@@ -14,23 +16,29 @@ function Header() {
     const res = await fetch(URL);
     const data = await res.json();
     setInfo(data);
-    console.log(data);
   };
 
   useEffect(() => {
     dataSheet();
   }, []);
 
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <nav className="navbar navbar-expand-md bg-body-tertiary bg-light justify-content-center text-center">
         <div className="container-fluid">
-          <a className="navbar-brand p-0" href="/">
+          <Link to="/">
+            <a className="navbar-brand p-0">
             <img src={logo} width="200" alt="corpohypatia" />
           </a>
+          </Link>
           <button
             className="navbar-toggler "
             type="button"
+            onClick={handleToggle}
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent"
@@ -40,36 +48,42 @@ function Header() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div
-            className="collapse navbar-collapse justify-content-end"
+            className={`collapse navbar-collapse justify-content-end ${isOpen ? "show" : ""}`}
             id="navbarSupportedContent"
           >
             <ul className="navbar-nav mb-lg-0 p-3 flex-column flex-sm-row justify-content-center">
               <li className="nav-item">
-                <a
-                  className="nav-link active fw-bold"
-                  aria-current="page"
-                  href="/nosotros-proyectos-colaboradores"
-                >
-                  <b>Nosotros</b>
-                </a>
+                <Link to="nosotros">
+                  <a
+                    className="nav-link active fw-bold"
+                    aria-current="page"
+                    onClick={handleToggle}
+                  >
+                    <b>Nosotros</b>
+                  </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a
-                  className="nav-link active fw-bold"
-                  aria-current="page"
-                  href="/proyectos-colaboradores"
-                >
-                  <b>Proyectos</b>
-                </a>
+                <Link to="proyectos">
+                  <a
+                    className="nav-link active fw-bold"
+                    aria-current="page"
+                    onClick={handleToggle}
+                  >
+                    <b>Proyectos</b>
+                  </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a
-                  className="nav-link active fw-bold"
-                  aria-current="page"
-                  href="/colaboradores"
-                >
-                  <b>Colaboradores</b>
-                </a>
+                <Link to="colaboradores">
+                  <a
+                    className="nav-link active fw-bold"
+                    aria-current="page"
+                    onClick={handleToggle}
+                  >
+                    <b>Colaboradores</b>
+                  </a>
+                </Link>
               </li>
               <li className="nav-item ms-sm-3">
                 <a
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,15 +3,38 @@ import Graphic from "../../assets/img/grafico.png";
 import "./home.css";
 import Header from "../../components/layouts/header/Header";
 import Footer from "../../components/layouts/footer/Footer";
+import {AiOutlineSearch} from "react-icons/ai"
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import { getAllProjects } from "../../service/api"
+import { useEffect, useState } from "react";
 
 function Home() {
+
+  const [allProyects, setAllProyects] = useState([])
+
+  useEffect(() => {
+    getAllProjects(setAllProyects)
+  }, []);
+  console.log(allProyects)
+
+  const testalert = () =>{
+    alert("probando")
+  }
+
   return (
     <>
       <Header />
       <div className="container-fluid">
         <div className="row p-5">
           <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 px-2">
-            <p className="fs-1 mt-5">BIENVENID@S</p>
+            <p className="fs-1 mt-5 welcome-title">BIENVENID@S</p>
             <p className=" mt-5">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit.
               Doloremque earum repudiandae velit ipsa molestiae perferendis quae
@@ -27,7 +50,7 @@ function Home() {
             </p>
             {/* buttons */}
             <div className="mt-5">
-              <button type="button" class="btn btn-lg btn-continuar">
+              <button type="button" className="btn btn-lg btn-continuar">
                 Continuar
               </button>
             </div>
@@ -45,28 +68,48 @@ function Home() {
         </div>
       </div>
 
-      {/* Title and search */}
-      <div className="row px-5 py-3 bg-purple text-purple align-items-center my-4">
+      {/* Projects itle and search */}
+      <div className="py-3 bg-purple text-purple align-items-center my-4">
         <div className="col-12 text-center">
-          <h1 className="text-center text-purple display-4 mb-4">Proyectos</h1>
+          <h1 className="text-center text-purple display-4 mb-4">PROYECTOS</h1>
         </div>
       </div>
+      <div className="search-bar">
+         <span><AiOutlineSearch/></span><input type={'search'} placeholder="Buscar un proyecto..."></input>
+      </div>
 
-      <div className="container-fluid">
-        <div className="row justify-content-start mb-3">
-          <div className="col-md-2 col-sm-2">
-            <div className="input-group input-group-sm">
-              <input
-                type="text"
-                className="form-control border-purple"
-                placeholder="Buscar proyectos"
-              />
-              <span className="input-group-text bg-purple border-purple">
-                <i className="bi bi-search text-white"></i>
-              </span>
-            </div>
-          </div>
-        </div>
+      {/* Card projects*/}
+
+      <div className="proyect-list">
+        {
+          allProyects.map(card =>{
+            return(
+              <div key={card._id}>
+                <Card sx={{ maxWidth: 330 }} className="card_body">
+                  <CardActionArea onClick={testalert}>
+                    <CardMedia
+                      component="img"
+                      alt="Proyect image"
+                      height="140"
+                      image={card.imagePath}
+                    />
+                  </CardActionArea>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div" className="project-title">
+                      {card.project_title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {card.project_location}
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button variant="contained" className="viewmore-butt">Leer más</Button>
+                  </CardActions>
+                </Card>
+              </div>
+            )
+          })
+        }
       </div>
 
       <Footer />
