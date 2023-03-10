fe68580385f9fee16e67d56d3527dd7b4a40bc8b
felipe veloza <ciberronaldo@hotmail.com>
2023-03-10 16:14:10 -0500

    projects sectión upgrades

--- src/components/layouts/404/NotFound.jsx
+++ src/components/layouts/404/NotFound.jsx
@@ -10,7 +10,7 @@ const NotFound = () => {
       <Header/>
       <div className="Not-Found-title">
         <h1 id="error">404 NOT FOUND</h1>
-        <h1 id="message">EY!! La pagina a la que intentas acceder al parecer no existe</h1>
+        <h1 id="message">Esta página no existe. <br></br><br></br>Comprueba la URL o regresa a la página de inicio de CorpoHypatia</h1>
         <img src={NotFoundImg}/>
         <Link to="/" id="link"><button>Regresar al inicio</button></Link>
       </div>
