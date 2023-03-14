5a0c24b27648bff439169a974967e979501b2b67
Andreinavlqz <andreinafloresvlqz54@gmail.com>
2023-03-14 11:50:46 -0500

    footer

--- src/components/layouts/404/NotFoundAdmin.jsx
+++ src/components/layouts/404/NotFoundAdmin.jsx
@@ -10,7 +10,7 @@ const NotFoundAdmin = () => {
       <div className="Not-Found-title">
         <h1 id="error">404 NOT FOUND</h1>
         <h1 id="message">Esta página no existe. <br></br><br></br>Comprueba la URL o regresa a la página de administración de CorpoHypatia</h1>
-        <img src={NotFoundImg}/>
+        <img src={NotFoundImg} alt="notfound"/>
         <Link to="/projects" id="link"><button>Regresar al inicio</button></Link>
       </div>
     </>
