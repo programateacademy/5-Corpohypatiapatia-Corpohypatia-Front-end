dac62dc9d0905e952865b7300e7630193e32738e
felipe veloza <ciberronaldo@hotmail.com>
2023-03-24 15:18:11 -0500

    Fix for CreateAdmins [Finalize]

--- src/pages/allUsers/AllUsers.jsx
+++ src/pages/allUsers/AllUsers.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-const URI = 'https://5-corpohypatiapatia-corpohypatia-back-end-ruddy.vercel.app'
+const URI = "https://5-corpohypatiapatia-corpohypatia-back-end-ruddy.vercel.app/users/";
 // const URI = "http://localhost:8000/users/"
 
 const AllUsers = () => {
